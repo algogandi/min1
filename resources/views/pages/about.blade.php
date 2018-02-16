1b18990098a4395619a564501be91bfe7726df42
abdullah <asa89@njit.edu>
2018-02-16 17:13:10 -0500

    min1

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+    <h1>THIS IS MY ABOUT PAGE</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+
+
+
+
+@endsection
